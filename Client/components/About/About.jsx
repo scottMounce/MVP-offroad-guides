09631f45d2bce121782d0cc3444346a41cbd6cda
Scott Mounce <scottmounce10@gmail.com>
2020-11-03 12:43:20 -0800

    added about

--- Client/components/About/About.jsx
+++ Client/components/About/About.jsx
@@ -1,12 +1,66 @@
 import React from 'react';
+import styles from'./about.module.css';
 
 const About = () => {
   return (
     <div>
-      
+      <div className={styles.whoWeAre}>Who we Are
+     <div>Project off-road allows us to connect the advanced off-road community to the new off-roader. </div>
+     </div>
+     
+     <div className={styles.offers}>
+     <div>Project Off-Road will define the next level of off-road prowess </div>
+     <div>We offer guides through some of the most known trails</div>
+     <div>We offer shop recommendations on where to get your next upgrades</div>
+     <div>We offer 1 on 1 consulting with advanced off-road industry professionals </div>
+     <div>We offer basic and advanced off-road and recovery training</div>
+     <div>We offer a free trial allowing access to basic 4x4 training videos</div> 
+     </div>
+     
+     <div className={styles.problem}>Problem
+     <div>Off-road and overlanding is gaining traction. As the number of new off-roaders increase we</div>
+     <div>need to establish a safe way of training and guidance through the advanced 4x4 community.</div>
+     </div>
+     <div className={styles.solution}>Solution
+     <div>Project Off-Road will only recommend and offer the best of the best for our users, the guides </div>
+     <div>will be local guides with advanced knowledge in safety, recovery, emergency medical training, </div>
+     <div>and the trails will be well known to them. Our consultants will point you in the right direction</div>
+     <div>so you only have to build your vehicle ONCE, and it will be everything to fit your needs. </div>
+     </div>
+     <div className={styles.joinNow}>
+     <div>Check out projectoff-road.com sign up for the free 7 day trial and get ready to experience the time of your life!</div>
+     </div>
     </div>
   )
    
 }
 
 export default About;
+
+{/* <div className={styles.whatAreWe}>
+<div>Project off-road allows us to connect the advanced off-road community to the new off-roader. </div>
+</div>  
+<div className={styles.summary}>
+<div>Project Off-Road will define the next level of off-road prowess </div>
+<div>We offer guides through some of the most known trails</div>
+<div>We offer shop recommendations on where to get your next upgrades</div>
+<div>We offer 1 on 1 consulting with advanced off-road industry professionals </div>
+<div>We offer basic and advanced off-road and recovery training</div>
+<div>We offer a free trial allowing access to basic 4x4 training videos</div>
+</div>  
+<div className={styles.problem}>
+ <div className={styles.titleProblem}>Problem</div>
+ <div>Off-road and overlanding is gaining traction. As the number of new off-roaders increase we</div>
+ <div>need to establish a safe way of training and guidance through the advanced 4x4 community.</div>
+</div>
+<div className={styles.solution}>
+<div className={styles.titleSolution}>Solution</div>
+<div>Project Off-Road will only recommend and offer the best of the best for our users, the guides </div>
+<div>will be local guides with advanced knowledge in safety, recovery, emergency medical training, </div>
+<div>and the trails will be well known to them. Our consultants will point you in the right direction</div>
+<div>so you only have to build your vehicle ONCE, and it will be everything to fit your needs. </div>
+</div>
+<div className={styles.trial}>
+<div>Check out projectoff-road.com sign up for the free 7 day trial and get ready to experience the time </div>
+<div>of your life! </div>
+</div>        */}
